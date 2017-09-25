60b2b56c1f87f41eeae68030b15e33cab206e71b
Brendan McIlhenny <bmcilhen@gmail.com>
2017-09-25 21:32:03 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,5 @@
 def reverse_each_word(sentence)
-  array = sentence.split()
-  new_array = array.collect do |word|
+  new_array = sentence.split().collect do |word|
     word.reverse
   end
   new_array.join(' ')
